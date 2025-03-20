37195add97594d7e386a8b9238d8a95e709b04be
ikabeee <carlglz30@gmail.com>
2025-03-20 09:22:06 -0500

    add Heroui components and update Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
+// eslint-disable-next-line no-undef
+const {heroui} = require('@heroui/theme');
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [heroui()],
 }
 
